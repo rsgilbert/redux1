d280a2281c8801a166062b28c35f47dbaa813ea1
rsgilbert <gssenyonjo@gmail.com>
2019-04-06 07:08:05 +0300

    redux start

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+ 
+const ExpenseDashboardPage = () => (
+    <div>
+        this is from the future boyww
+    </div>
+)
+const AddExpensePage = () => (
+    <div>
+        i will get your salary
+    </div>
+)
+const EditExpensePage = () => (
+    <div>
+        I will edit uou
+    </div>
+)
+const HelpPage = () => (
+    <div>
+        Thr lord of Helpers
+    </div>
+)
+const NotFoundPage = () => (
+    <div>
+        404 here
+    </div>
+
+)
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" component={ ExpenseDashboardPage } exact={true}/>
+            <Route path ="/create" component={ AddExpensePage } exact={true}/>
+            <Route path = "/edit" component={ EditExpensePage } />
+            <Route path= "/help" component={ HelpPage } />
+            <Route component={NotFoundPage}/>
+        </Switch>
+       
+    </BrowserRouter>
+)
+
+
+ReactDOM.render((
+    routes
+), document.getElementById('app'))
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// entry -> output
+
+const path = require('path')
+
+module.exports = { 
+    entry: "./src/app.js"
+    ,output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+    // how you want to use your loader
+    ,module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+    ,devtool: 'cheap-module-eval-source-map'
+    ,devServer: {
+        contentBase: path.join(__dirname, "public")
+        ,historyApiFallback: true // means will be handling all routing via client side code and should return index for all 404 routes
+        
+    }
+}
+// historyApiFallback: true // means will be handling all routing via client side code and should return index for all 404 routes
+// read more about regular expressions
